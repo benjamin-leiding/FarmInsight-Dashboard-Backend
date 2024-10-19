30df26832945e44bf84e13ac451f73dd7ac598aa
Julian Schöpe <julian.schoepe@web.de>
2024-10-19 12:56:37 +0200

    setup django project

---
+++ django_server/farminsight_dashboard_backend/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class FarminsightDashboardBackendConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'farminsight_dashboard_backend'
