53e2344d6f88240159876595ee73ff94497ad50c
Julian Schöpe <j.schoepe@ostfalia.de>
2024-10-27 17:05:46 +0100

    brought organization return types inline with api description

--- django_server/farminsight_dashboard_backend/views/organization_views.py
+++ django_server/farminsight_dashboard_backend/views/organization_views.py
@@ -10,18 +10,10 @@ from ..services.organization_services import create_organization
 @permission_classes([IsAuthenticated])
 def post_organization(request):
     org = create_organization(request.data["name"], request.data["isPublic"], request.user)
-    memberships = Membership.objects.filter(organization=org).prefetch_related('userprofile')
     data = {
         "id": org.id,
         "name": org.name,
         "isPublic": org.isPublic,
-        "memberships": [{
-            "id": membership.id,
-            "role": membership.membershipRole,
-            "userprofile": {
-                "name": membership.userprofile.name,
-            }
-        } for membership in memberships]
     }
     return Response(data, status=status.HTTP_201_CREATED)
 
@@ -30,13 +22,14 @@ def post_organization(request):
 @permission_classes([IsAuthenticated])
 def get_own_organizations(request):
     memberships = Membership.objects.filter(userprofile_id=request.user.id).prefetch_related('organization').all()
-    orgs = set((membership.organization, membership.membershipRole) for membership in memberships)
     data = []
-    for org, role in orgs:
+    for membership in memberships:
         data.append({
-            'id': org.id,
-            'name': org.name,
-            'isPublic': org.isPublic,
-            'membershipRole': role,
+            'id': membership.organization.id,
+            'name': membership.organization.name,
+            'membership': {
+                'id': membership.id,
+                'role': membership.membershipRole,
+            }
         })
     return Response(data)
