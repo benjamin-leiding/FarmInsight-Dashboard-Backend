c0e4fdc508804c1d448db64e87a6d8027b3e8c65
Julian Schöpe <j.schoepe@ostfalia.de>
2024-11-03 15:11:24 +0100

    SystemAdmin can also add Users to any Organization

--- django_server/farminsight_dashboard_backend/services/membership_services.py
+++ django_server/farminsight_dashboard_backend/services/membership_services.py
@@ -2,7 +2,7 @@ from django.db.models import QuerySet
 from django.core.exceptions import PermissionDenied
 
 from farminsight_dashboard_backend.serializers import MembershipSerializer
-from farminsight_dashboard_backend.models import Userprofile, Membership, MembershipRole
+from farminsight_dashboard_backend.models import Userprofile, Membership, MembershipRole, SystemRole
 
 
 def get_memberships(user: Userprofile) -> QuerySet[Membership]:
@@ -10,12 +10,12 @@ def get_memberships(user: Userprofile) -> QuerySet[Membership]:
 
 
 def create_membership(creating_user: Userprofile, data: dict) -> MembershipSerializer:
-    # Check that the one adding a Member is an Admin of the Organization
+    # Check that the one adding a Member is an Admin of the Organization, or System Admin of the Backend
     memberships = get_memberships(creating_user)\
         .filter(organization_id=data['organizationId'], membershipRole=MembershipRole.Admin.value)\
         .all()
 
-    if len(memberships) > 0:
+    if len(memberships) > 0 or creating_user.systemRole == SystemRole.SystemAdmin.value:
         membership_serializer = MembershipSerializer(data=data)
         if membership_serializer.is_valid(raise_exception=True):
             membership_serializer.save()
