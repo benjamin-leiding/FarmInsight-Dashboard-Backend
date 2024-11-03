1fb392b9dd58bf18f446c0e03a998c641e3ad240
Julian Schöpe <j.schoepe@ostfalia.de>
2024-11-03 15:04:56 +0100

    make organization name unique

--- django_server/farminsight_dashboard_backend/models/organization.py
+++ django_server/farminsight_dashboard_backend/models/organization.py
@@ -4,5 +4,5 @@ from django.db import models
 
 class Organization(models.Model):
     id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
-    name = models.CharField(max_length=256)
+    name = models.CharField(max_length=256, unique=True)
     isPublic = models.BooleanField(default=False)
