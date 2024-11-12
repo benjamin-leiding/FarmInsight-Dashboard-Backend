8c911df0ed148779bcca36af5f8012adc7e913f4
Julian Schöpe <j.schoepe@ostfalia.de>
2024-11-12 15:37:18 +0100

    changed url order

--- django_server/farminsight_dashboard_backend/urls.py
+++ django_server/farminsight_dashboard_backend/urls.py
@@ -16,10 +16,10 @@ from farminsight_dashboard_backend.views import (
 urlpatterns = [
     path('userprofiles', get_userprofile, name='get_userprofile'),
     path('userprofiles/<str:search_string>', get_userprofile_by_search_string, name='get_userprofile_by_search_string'),
+    path('organizations/own', get_own_organizations, name='get_own_organizations'),
     path('organizations', post_organization, name='post_organization'),
     path('organizations/<str:organization_identifier>', get_organization, name='get_organization'),
     path('memberships', post_membership, name='post_membership'),
-    path('organizations/own', get_own_organizations, name='get_own_organizations'),
     path('fpfs', post_fpf, name='post_fpf'),
     path('fpfs/<str:fpf_id>/data', get_fpf_data, name='get_fpf_data'),
     path('sensors/<str:sensor_id>/measurements', get_sensor_data, name='get_sensor_data'),
