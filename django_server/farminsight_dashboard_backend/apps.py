556fd150bf102871ce7a246809481fbe6626e0ed
Marius <mar.peter@ostfalia.de>
2024-12-01 14:38:33 +0100

    fix bug on startup

--- django_server/farminsight_dashboard_backend/apps.py
+++ django_server/farminsight_dashboard_backend/apps.py
@@ -1,8 +1,11 @@
+import logging
+import time
 from django.apps import AppConfig
 import os
 import threading
-
-from django.db.models.signals import post_migrate
+from django.db.utils import OperationalError
+from django.db.migrations.executor import MigrationExecutor
+from django.db import connections
 
 
 class FarminsightDashboardBackendConfig(AppConfig):
@@ -11,23 +14,55 @@ class FarminsightDashboardBackendConfig(AppConfig):
 
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
+        self.log = logging.getLogger('farminsight_dashboard_backend')
+
+    def initialize_app(self, max_retries=3, retry_interval=3):
+        """
+        :param max_retries: maximum amount of retries to connect to the database
+        :param retry_interval: interval of the retry
+        """
+        retry_count = 0
+        while retry_count < max_retries:
+            try:
+                if self.has_pending_migrations():
+                    self.log.warning(f"Pending migrations detected. Retrying in {retry_interval} seconds...")
+                    time.sleep(retry_interval)
+                    retry_count += 1
+                else:
+                    from farminsight_dashboard_backend.services import InfluxDBManager, CameraScheduler
+
+                    InfluxDBManager.get_instance().initialize_connection()
+                    CameraScheduler.get_instance().start()
+                    self.log.info("Started successfully.")
+                    break
+            except OperationalError as e:
+                self.log.error(f"Database not ready yet: {e}")
+                time.sleep(retry_interval)
+                retry_count += 1
+            except Exception as e:
+                self.log.error(f"Error checking migrations: {e}")
+                break
+        if retry_count == max_retries:
+            self.log.error("Max retries reached. App did not start.")
+
+    def has_pending_migrations(self) -> bool:
+        """
+        Check if there are any pending migrations.
+        :return: if there are pending migrations
+        """
+        try:
+            executor = MigrationExecutor(connections['default'])
+            targets = executor.loader.graph.leaf_nodes()
+            return executor.migration_plan(targets) != []
+
+        except Exception as e:
+            self.log.error(f"Error checking migrations: {e}")
+            return True  # Assume pending if there's an error
 
     def ready(self):
         """
-        Start a new thread to check for pending migrations and start the scheduler if ready
+        Start a new thread to check for pending migrations and start the app if ready
         """
-
         if os.environ.get('RUN_MAIN') == 'true':
-            from farminsight_dashboard_backend.services import InfluxDBManager, CameraScheduler
-            from django.db import connections
+            threading.Thread(target=self.initialize_app, daemon=True).start()
 
-            def initialize_services(sender, **kwargs):
-                if connections['default'].is_usable():
-                    threading.Thread(
-                        target=InfluxDBManager.get_instance().initialize_connection, daemon=True
-                    ).start()
-                    threading.Thread(
-                        target=CameraScheduler.get_instance().start, daemon=True
-                    ).start()
-
-            post_migrate.connect(initialize_services, sender=self)
