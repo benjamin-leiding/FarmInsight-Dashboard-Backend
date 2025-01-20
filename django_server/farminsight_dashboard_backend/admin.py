d856ef4a203ae7c6aef35e8f505a157e6ba0d557
julian <j.schoepe@ostfalia.de>
2025-01-20 14:50:38 +0100

    enable admin access to user, membership and orga

--- django_server/farminsight_dashboard_backend/admin.py
+++ django_server/farminsight_dashboard_backend/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
 
+from farminsight_dashboard_backend.models import Userprofile, Membership, Organization
+
 # Register your models here.
+admin.site.register(Userprofile)
+admin.site.register(Membership)
+admin.site.register(Organization)
